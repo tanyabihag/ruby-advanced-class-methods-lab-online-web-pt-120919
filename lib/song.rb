b933ab8474bdbfcf5927bbf279b732f7e1657a4e
Tanya Bihag <tbihag@gmail.com>
2020-01-06 23:14:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,36 @@
 class Song
-  attr_accessor :name, :artist_name
+attr_accessor :name, :artist_name
   @@all = []
+     
+def self.all
+  @@all
+end
+     
+def save
+ self.class.all << self 
+end
 
-  def self.all
-    @@all
-  end
+def self.create
+  song = self.new 
+  @@all << song 
+  song
+end
 
-  def save
-    self.class.all << self
-  end
+def self.new_by_name(name)
+  song = self.new
+  song.name = name
+  song
+end
+
+def self.create_by_name(name)
+  song = self.new
+  song.name = name
+  @@all << song
+  song
+end
+
+def self.find_by_name(name)
+  @@all.find{|x| x.name == name}
+end
 
 end
