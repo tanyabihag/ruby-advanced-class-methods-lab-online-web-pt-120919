74a517d6c9acaf47202078c85a854d773c762254
Tanya Bihag <tbihag@gmail.com>
2020-01-06 23:19:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,10 +35,10 @@ end
 
 
 def self.find_or_create_by_name(name)
-   if self.find_by_name
-     self.find_by_name
+   if self.find_by_name(name)
+     self.find_by_name(name)
     else
-      self.create_by_name
+      self.create_by_name(name)
   end
 end
   
